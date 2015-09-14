64a24fcaa7a60a81c21e49520cb1503d0cae8788
Daniel Benediktsson <danielb13@ru.is>
2015-09-14 18:03:02 +0000

    flot

--- src/tftpd.c
+++ src/tftpd.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     /* Network functions need arguments in network byte order instead of
        host byte order. The macros htonl, htons convert the values, */
     server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(32000);
+    server.sin_port = htons(argc);
     bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
 
     for (;;) {
